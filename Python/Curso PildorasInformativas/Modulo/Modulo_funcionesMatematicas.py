f6e8553b1222a8d95d6e1e75d871ee71e522a558
alexperdel <aperdel@gmail.com>
2025-08-25 08:26:31 +0200

    chore: remove old test projects

--- Python/Curso PildorasInformativas/Modulo/Modulo_funcionesMatematicas.py
+++
@@ -1,8 +0,0 @@
-def sumar (op1, op2):
-    print("El resultado es ", op1+op2)
-    
-def restar (op1, op2):
-    print("El resultado es ", op1-op2)
-    
-def multiplicar (op1, op2):
-    print("El resultado es ", op1*op2)
--- Python/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="Modulo_funcionesMatematicas",
-    version="1.0",
-    description="Paquete de funciones",
-    author="Alex",
-    packages=find_packages()
-)
